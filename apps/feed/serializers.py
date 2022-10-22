3bb6d006bafed5c9de43f0b72b5555ffad7cd792
vitor <vpbohn@gmail.com>
2022-10-22 18:15:45 -0300

    images

--- apps/feed/serializers.py
+++ apps/feed/serializers.py
@@ -4,7 +4,8 @@ from .models import Tweek
 
 class TweekSerializer(serializers.ModelSerializer):
     tweeker_name = serializers.CharField(source='created_by')
+    avatar_url = serializers.CharField(source='created_by.twikkerprofile.avatar.url')
 
     class Meta:
         model = Tweek
-        fields = ('id', 'body', 'created_by', 'tweeker_name')
+        fields = ('id', 'body', 'created_by', 'tweeker_name', 'avatar_url')
