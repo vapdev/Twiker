210e1af7c93becabe02d44c97f057f9cd46b2377
vitor <vpbohn@gmail.com>
2022-10-27 23:54:27 -0300

    it is working

--- apps/twikkerprofile/models.py
+++ apps/twikkerprofile/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class TwikkerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
-    avatar = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    avatar = models.ImageField(upload_to='uploads/', blank=True, null=True, default='uploads/default.png')
 
     def __str__(self):
         return self.user.username
