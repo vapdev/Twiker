08e761b9ecf9835e5bb40bf3ae3da3607c351d50
vitor <vpbohn@gmail.com>
2022-11-06 02:31:03 -0300

    dark mode alpha

--- static/js/utils.mjs
+++ static/js/utils.mjs
@@ -1,8 +1,6 @@
 export const getVariableFromDjango = function (variableId) {
-    console.log("variableId", variableId);
     const element = document.querySelector(`#${variableId}`);
     if (element) {
-        console.log("element", element.textContent);
         return JSON.parse(element.innerText);
     }
 }
--- twikker/settings.py
+++ twikker/settings.py
@@ -131,6 +131,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = 'var/static_root/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
