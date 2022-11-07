d512d5a3261be87f4f738beec4e652dc86ce76e3
vitor <vpbohn@gmail.com>
2022-11-07 00:11:16 -0300

    admin view

--- apps/feed/admin.py
+++ apps/feed/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Tweek
+from .models import Tweek, Dislike, Like
 
 admin.site.register(Tweek)
+admin.site.register(Like)
+admin.site.register(Dislike)
