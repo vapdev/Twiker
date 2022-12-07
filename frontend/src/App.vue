d328e785aa22d56327e04c127a596ae62c73283b
vitor <vpbohn@gmail.com>
2022-12-07 00:00:05 -0300

    fix auth session

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,7 @@ import axios from 'axios'
 export default {
   name: 'App',
   beforeCreate(){
-    this.$store.commit('initiateStore')
+    this.$store.commit('initializeStore')
 
     const token = this.$store.state.token
 
