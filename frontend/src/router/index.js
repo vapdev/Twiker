ec0a9dafa4cb5028de888fac0790ecd154f30da8
DenLopes <denis.gnl@gmail.com>
2022-12-22 16:39:28 -0300

    Changed the structure of views using nested routers

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,11 @@
 import { createWebHistory, createRouter } from "vue-router"
-import Feed from "../views/Feed.vue"
-import Conversations from "../views/Conversations.vue"
-import Notifications from "../views/Notifications.vue"
-import Profile from "../views/Profile.vue"
-import Users from "../views/Users.vue"
+import Main from "../views/Main.vue"
+import Feed from "../components/Feed.vue"
+import Conversations from "../components/Conversations.vue"
+import Notifications from "../components/Notifications.vue"
+import Profile from "../components/Profile.vue"
+import Users from "../components/Users.vue"
+import Globalchat from "../components/GlobalChat.vue"
 
 const routes = [
   {
@@ -18,51 +20,58 @@ const routes = [
   },
   {
     path: "/",
-    name: "Feed",
-    component: Feed,
+    name: "Main",
+    component: Main,
+    children: [
+      {
+        path: "/feed",
+        name: "Feed",
+        component: Feed,
+      },
+      {
+        path: "/conversations",
+        name: "Conversations",
+        component: Conversations,
+      },
+      {
+        path: "/conversation/:id",
+        name: "Conversation",
+        props: true,
+        component: () => import("../components/Conversation.vue")
+      },
+      {
+        path: "/notifications",
+        name: "Notifications",
+        component: Notifications
+      },
+      {
+        path: "/users",
+        name: "Users",
+        component: Users
+      },
+      {
+        path: "/profile",
+        name: "SelfProfile",
+        component: Profile
+      },
+      {
+        path: "/profile/:username",
+        name: "Profile",
+        component: Profile
+      },
+      {
+        path: "/globalchat",
+        name: "GlobalChat",
+        component: Globalchat
+      },
+      {
+        path: "/tweek/:id",
+        name: "Tweek",
+        props: true,
+        component: () => import("../components/TweekPage.vue")
+      },
+    ]
   },
-  {
-    path: "/conversations",
-    name: "Conversations",
-    component: Conversations
-  },
-  {
-    path: "/conversation/:id",
-    name: "Conversation",
-    props: true,
-    component: () => import("../views/Conversation.vue")
-  },
-  {
-    path: "/notifications",
-    name: "Notifications",
-    component: Notifications
-  },
-  {
-    path: "/users",
-    name: "Users",
-    component: Users
-  },
-  {
-    path: "/profile",
-    name: "SelfProfile",
-    component: Profile
-  },
-  {
-    path: "/profile/:username",
-    name: "Profile",
-    component: Profile
-  },
-  {
-    path: "/globalchat",
-    name: "GlobalChat",
-    component: () => import("../views/GlobalChat.vue")
-  },
-  {
-    path: "/tweek/:id",
-    name: "Tweek",
-    props: true,
-    component: () => import("../components/TweekPage.vue")
-  }
 ]
 
 const router = createRouter({
