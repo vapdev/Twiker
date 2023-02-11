9ac9f00b6528801f9dfd29cc3c94e81ac26439ef
vitor <vpbohn@gmail.com>
2023-02-10 23:37:36 -0300

    build: asgi file

--- twikker/asgi.py
+++ twikker/asgi.py
@@ -1,16 +1,17 @@
 import os
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twikker.settings')
+import django
+django.setup()
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-
 from django.core.asgi import get_asgi_application
 
 import apps.conversation.routing as conversation_routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twikker.settings')
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             conversation_routing.websocket_urlpatterns
