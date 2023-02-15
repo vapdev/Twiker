f26df87048352bf9901816e60a5c417b01aa3b66
vitor <vpbohn@gmail.com>
2023-02-15 20:55:32 -0300

    FIX: fix

--- apps/twikkerprofile/views.py
+++ apps/twikkerprofile/views.py
@@ -34,7 +34,7 @@ def get_user_data(request, username):
 
 @permission_classes((IsAuthenticated, ))
 @api_view(['GET'])
-def get_profile_data(request):
+def get_profile_data(request, username):
     user = get_object_or_404(User, username=request.user.username)
     serializer = TwikkerProfileSerializer(user)
     return JsonResponse(serializer.data)
