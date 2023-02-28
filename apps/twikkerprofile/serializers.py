191f5b6b3dd21f3e2523dc20d2b90e24299369a9
vitor <vpbohn@gmail.com>
2023-02-28 00:38:58 -0300

    tiro no escuro

--- apps/twikkerprofile/serializers.py
+++ apps/twikkerprofile/serializers.py
@@ -9,6 +9,13 @@ class TwikkerProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username', read_only=True)
     followed_by = serializers.IntegerField(source='followed_by.count', read_only=True)
     following = serializers.IntegerField(source='follows.count', read_only=True)
+    avatar = serializers.SerializerMethodField('get_avatar_url')
+    
+    def get_avatar_url(self, twikker_profile):
+        if twikker_profile.avatar:
+            return twikker_profile.avatar.url.replace('image/upload/', '')
+        return None
+    
     class Meta:
         model = TwikkerProfile
         fields = '__all__'
