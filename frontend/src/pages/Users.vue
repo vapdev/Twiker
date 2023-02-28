56c6b937d1df7204ad84d4cdf4d179ec171d88f6
vitor <vpbohn@gmail.com>
2023-02-27 21:40:50 -0300

    STYLE: ajuste visual usuarios

--- frontend/src/pages/Users.vue
+++ frontend/src/pages/Users.vue
@@ -1,36 +1,19 @@
 <template>
   <div
-    class="flex flex-col w-full border-solid border-x-2 border-gray-100 dark:border-gray-700 max-[600px]:border-x-0 max-[600px]:mb-14"
-  >
-    <DefaultHeader
-      :mainText="'Todos Usuários'"
-      :sideText="'Usuários: ' + users.length"
-    />
+    class="flex flex-col w-full border-solid border-x-2 border-gray-100 dark:border-gray-700 max-[600px]:border-x-0 max-[600px]:mb-14">
+    <DefaultHeader :mainText="'Todos Usuários'" :sideText="'Usuários: ' + users.length" />
     <div class="flex flex-col justify-center">
-        <form method="GET" action="" class="m-0" @submit.prevent="getUsers">
-          <div
-            class="flex flex-row p-2 my-2 max-[600px]:my-0 border-solid border-2 border-gray-100 dark:border-gray-700 w-[200px] rounded-full"
-          >
-            <div>
-              <input
-                class="text-sm h-fit w-full outline-none bg-white dark:bg-dark"
-                type="text"
-                name="query"
-                v-model="searchQuery"
-                placeholder="Search"
-              />
-            </div>
-            <div>
-              <button><i class="p-1 fa-solid fa-magnifying-glass"></i></button>
-            </div>
-          </div>
-        </form>
-      </div>
-    <router-link
-      :to="`/profile/${user.username}`"
-      v-for="user in users"
-      class="flex flex-row h-fit w-full p-2 pt-3 pl-3 border-solid border-b-2 hover:bg-gray-100 dark:hover:bg-gray-700 border-gray-100 dark:border-gray-700"
-    >
+      <form method="GET" action="" class="m-0" @submit.prevent="getUsers">
+        <div
+          class="flex ml-2 flex-row p-2 my-2 max-[600px]:my-0 border-solid border-2 border-gray-100 dark:border-gray-700 w-[200px] rounded-full">
+          <input class="text-sm h-fit w-full outline-none bg-white dark:bg-dark" type="text" name="query"
+            v-model="searchQuery" placeholder="Search" />
+          <button><i class="p-1 fa-solid fa-magnifying-glass"></i></button>
+        </div>
+      </form>
+    </div>
+    <router-link :to="`/profile/${user.username}`" v-for="user in users"
+      class="flex flex-row h-fit w-full p-2 pt-3 pl-3 border-solid border-b-2 hover:bg-gray-100 dark:hover:bg-gray-700 border-gray-100 dark:border-gray-700">
       <Avatar :avatar_url="user.avatar" class="mr-3" />
       <div class="flex flex-col">
         <div class="flex">
